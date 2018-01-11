26e2549ab7015b973f9fe1e6527d99e96e16765a
aoreskovic <ante.oreskovic@outlook.com>
2018-01-11 21:08:18 +0100

    lcd is working, added icons for rain, sun and clouds

--- STM_Discovery_Code/main.c
+++ STM_Discovery_Code/main.c
@@ -8,6 +8,8 @@
 #include <stm32f4_discovery.h>
 
 #include <main.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 // forward declarations of task functions
 void vTask1(void *pvParameters);
@@ -69,18 +71,36 @@ int main(void)
     uint8_t data;
     //Describe special character:
     uint8_t heart[8] = {0x0, 0xa, 0x1f, 0x1f, 0xe, 0x4, 0x0};
+    uint8_t drop[8] = {0x04, 0x04, 0x0E, 0x0E, 0x1F, 0x1F, 0x1F, 0x0E};
+    uint8_t sun_l[8] = { 0x08, 0x04, 0x01, 0x03, 0x1B, 0x03, 0x01, 0x04};
+    uint8_t sun_d[8] = { 0x02, 0x04, 0x10, 0x18, 0x1B, 0x18, 0x10, 0x04};
+    uint8_t obl_l[8] = {0x00, 0x03, 0x04, 0x0C, 0x12, 0x10, 0x0F, 0x00};
+    uint8_t obl_d[8] = { 0x00, 0x10, 0x0C, 0x12, 0x01, 0x01, 0x1E, 0x00};
     int i;
     int x;
     char buf[10];
 
-    LCDI2C_init(0x27, 16, 2); //Setup for I2C address 27, 16x2 LCD.
+    LCDI2C_init(0x27, 20, 4); //Setup for I2C address 27, 16x2 LCD.
 
     // Create and write special character at location 0.
     //  Locations 0-7 are available in the 44780 controller:
     LCDI2C_createChar(0, heart);
-    // It was necessary to do a LCDI2C_clear after the char definition.
-    // Probably a quirk of the library.
     LCDI2C_clear();
+    LCDI2C_createChar(1, drop);
+    // It was necessary to do a LCDI2C_clear after the char definition.
+    LCDI2C_clear();
+
+    LCDI2C_createChar(2, sun_l);
+    LCDI2C_clear();
+    LCDI2C_createChar(3, sun_d);
+    LCDI2C_clear();
+
+    LCDI2C_createChar(4, obl_l);
+    LCDI2C_clear();
+    LCDI2C_createChar(5, obl_d);
+    LCDI2C_clear();
+
+
 
     // -------  blink backlight  -------------
     LCDI2C_backlight(); //Turn on Backlight
@@ -89,10 +109,15 @@ int main(void)
     Delay(100);
     LCDI2C_backlight(); //Turn on Backlight
 
-    LCDI2C_write_String("STM32F103 ");
+    LCDI2C_write_String("Ikone: ");
 
     LCDI2C_write(0); //Write the special character at the next available location.
     // Another ASCII character can also be written.
+    LCDI2C_write(1);
+    LCDI2C_write(2);
+    LCDI2C_write(3);
+    LCDI2C_write(4);
+    LCDI2C_write(5);
 
     //Set cursor position. (column, line) Top line is 0, bottom is 1.
     LCDI2C_setCursor(0, 1);
@@ -100,17 +125,31 @@ int main(void)
     //Write an integer:
     LCDI2C_write_String("ARM-GCC  x=");
     x = 0;
-
     while (1)
     {
         itoa(x, buf, 16); // itoa takes number base as 3rd argument. Here; hex.
         LCDI2C_setCursor(11, 1);
         LCDI2C_write_String(buf);
         LCDI2C_write_String("  ");
+        //LCDI2C_setCursor(18, x % 4 + 1);
+        //LCDI2C_write_String("  ");
         x = x + 1;
-        Delay(200);
-        if (x > 255)
+        if (x > 3)
             x = 0;
+
+        LCDI2C_setCursor(15, (x + 1) % 4);
+        LCDI2C_write(1);
+        LCDI2C_setCursor(16, (x + 2) % 4);
+        LCDI2C_write(1);
+        LCDI2C_setCursor(17, (x + 0) % 4);
+        LCDI2C_write(1);
+        Delay(500);
+        LCDI2C_setCursor(15, (x + 1) % 4);
+        LCDI2C_write_String(" ");
+        LCDI2C_setCursor(16, (x + 2) % 4);
+        LCDI2C_write_String(" ");
+        LCDI2C_setCursor(17, (x + 0) % 4);
+        LCDI2C_write_String(" ");
     }
 
     gpio_init();
